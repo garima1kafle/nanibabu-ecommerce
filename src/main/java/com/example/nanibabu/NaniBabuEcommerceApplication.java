c2951ccc58df2c85bb557bf80c35ee9576850ebe
garima <kaamxamero@gmail.com>
2024-09-03 08:30:29 +0545

    init project

---
+++ src/main/java/com/example/nanibabu/NaniBabuEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.nanibabu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaniBabuEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaniBabuEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nanibabu/NaniBabuEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nanibabu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaniBabuEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
